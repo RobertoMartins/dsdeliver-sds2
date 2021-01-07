e9bfaee08277eab4550b99edab8e1e32d518e733
Roberto Murilo Martins Cordeiro <robertomartins@users.noreply.github.com>
2021-01-07 12:25:14 -0300

    Add Home page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css';
 import Navbar from './Navbar';
 import './App.css'
+import Home from './Home'
 
 function App() {
   return (
     <div>
      <Navbar/>
+     <Home/>
     </div>
   );
 }
